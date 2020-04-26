0d60a43575d773592572d3130c02dabfa2ee10fb
LeNiglo <lefrantguillaume@gmail.com>
2020-04-26 15:06:18 +0200

    WIP: dashboard widgets, need a brand new implementation in both backoffice and API

---
+++ app/View/Components/Widgets/YoungsWidget.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\View\Components\Widgets;
+
+use App\Http\Wrapper\HeleApiWrapper;
+use Illuminate\View\Component;
+
+class YoungsWidget extends Component
+{
+    /**
+     * An instance of HeleApiWrapper.
+     *
+     * @var HeleApiWrapper
+     */
+    private $hele = null;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->hele = new HeleApiWrapper();
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        $users = $this->hele->call('users.youngs_index', []);
+
+        return view('components.widgets.count-widget', [
+            'count' => $users['total'],
+            'entity' => __('Jeunes'),
+        ]);
+    }
+}
